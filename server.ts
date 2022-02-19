815560695af2743949af0b0df205699121d16ff2
JungAn2 <92061566+JungAn2@users.noreply.github.com>
2022-02-18 19:48:32 -0500

    Initial commit

---
+++ server.ts
@@ -0,0 +1,26 @@
+// Import config to setup the global configuration object.
+import { config } from './config'
+import express from 'express';
+
+const app = express();
+
+/** Import routers from ``app/routes/index`` **/
+import indexRouter from './app/routes/index';
+
+/** Configure application to use routers **/
+app.use('/', indexRouter);
+
+/** Global Middleware Example */
+import ExampleMiddleware from './app/http/middleware/ExampleMiddleware'
+
+/** Using the middleware on all routes */
+app.use(ExampleMiddleware);
+
+/** Using a custom error handler */
+import ExceptionHandler from './app/exceptions/ExceptionHandler';
+app.use(ExceptionHandler);
+
+/** Fire up server **/
+app.listen(config.app.port, () => {
+  console.log("Server started");
+});
