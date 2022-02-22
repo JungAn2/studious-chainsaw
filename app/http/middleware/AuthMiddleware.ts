7368104f5736022591ca939ab81700b4c1c5a34a
JungAn2 <jungan1991@gmail.com>
2022-02-21 22:56:21 -0500

    tried T.T

--- app/http/middleware/AuthMiddleware.ts
+++ app/http/middleware/AuthMiddleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import {AuthService} from '../../services/AuthService'
 import UnauthorizedException from '../../exceptions/UnauthorizedException'
+import {jwt} from 'jsonwebtoken'
+
 
 /**
  * An example of authentication middleware to create authorized views / routes.
@@ -9,6 +11,7 @@ import UnauthorizedException from '../../exceptions/UnauthorizedException'
  * @param res {Response} Express response object
  * @param next {NextFunction} Express next function
  */
+/*
 export default async function (req: Request, res: Response, next: NextFunction) {
 	// Check request headers for authorization token of some kind
 	// We'll just assume the entire authorization header is the token for the example.
@@ -20,5 +23,21 @@ export default async function (req: Request, res: Response, next: NextFunction) 
 	}
 
 	// Go to the next middleware / controller
+	return next()
+}*/
+
+export default async function (req: Request, res: Response, next: NextFunction) {
+	const authHeader = req.headers.authorization
+	//Check if authHeader exists
+	const token = authHeader && authHeader.split(' ')[1]
+	if(token == null){
+		res.sendStatus(401)
+	}
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=> {
+		if (err){
+			res.sendStatus(401)
+		}
+		req.user
+	})
 	return next()
 }
--- config/index.ts
+++ config/index.ts
@@ -3,7 +3,6 @@ import 'dotenv/config';
 // Import configuration objects
 import app from './app';
 
-
 /** Configuration loaded from ``config/`` & environmental variables */
 export const config = {
 	app
