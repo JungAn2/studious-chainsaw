5087304381498d5d5c5c566fdc0c31ddc0041d21
JungAn2 <jungan1991@gmail.com>
2022-02-22 23:29:26 -0500

    create user without input validations

--- app/http/middleware/AuthMiddleware.ts
+++ app/http/middleware/AuthMiddleware.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import {AuthService} from '../../services/AuthService'
 import UnauthorizedException from '../../exceptions/UnauthorizedException'
-import {jwt} from 'jsonwebtoken'
-
 
 /**
  * An example of authentication middleware to create authorized views / routes.
@@ -11,33 +9,17 @@ import {jwt} from 'jsonwebtoken'
  * @param res {Response} Express response object
  * @param next {NextFunction} Express next function
  */
-/*
 export default async function (req: Request, res: Response, next: NextFunction) {
 	// Check request headers for authorization token of some kind
 	// We'll just assume the entire authorization header is the token for the example.
-	const token = req.headers.authorization ? req.headers.authorization : "";  // auth header or empty string.
+	const token = req.headers.authorization.split(" ")[1] ? req.headers.authorization : "";  // auth header or empty string.
 
 	// If the auth service doesn't validate the user	
 	if (!AuthService.validate(token)) {
 		return next(new UnauthorizedException());
 	}
+	
 
 	// Go to the next middleware / controller
-	return next()
-}*/
-
-export default async function (req: Request, res: Response, next: NextFunction) {
-	const authHeader = req.headers.authorization
-	//Check if authHeader exists
-	const token = authHeader && authHeader.split(' ')[1]
-	if(token == null){
-		res.sendStatus(401)
-	}
-	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user)=> {
-		if (err){
-			res.sendStatus(401)
-		}
-		req.user
-	})
 	return next()
 }
